74249ac172e4f279405dc7460f87597aec92f5f6
gmlwn4009 <77002728+gmlwn4009@users.noreply.github.com>
2021-02-01 13:47:36 +0900

    Create main_activity.java

---
+++ main_activity.java
@@ -0,0 +1,26 @@
+package com.example.daily_weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.ListFragment;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        FragmentManager manager = getSupportFragmentManager();
+        fragment = manager.findFragmentById(R.id.main_fragment);
+
+    }
+}
